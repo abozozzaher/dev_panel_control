10e5e5e4c8046f1cc033af498d6deb19164eafc7
abozozzaher <gegak124@gmail.com>
2024-11-09 12:55:36 +0300

    V- 2.0.3

--- lib/pages/product/proInvPage.dart
+++ lib/pages/product/proInvPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:path/path.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
